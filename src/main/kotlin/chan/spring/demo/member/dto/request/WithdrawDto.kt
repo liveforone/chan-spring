ff083b1abb74069dbf4466404112a3b86e086790
liveforone <yc4852@gmail.com>
2024-04-22 23:24:58 +0900

    Refactor rename all request dto. Match to convention

--- src/main/kotlin/chan/spring/demo/member/dto/request/WithdrawDto.kt
+++ src/main/kotlin/chan/spring/demo/member/dto/request/WithdrawDto.kt
@@ -2,7 +2,6 @@ package chan.spring.demo.member.dto.request
 
 import jakarta.validation.constraints.NotBlank
 
-data class RecoveryRequest(
-    @field:NotBlank(message = "이메일을 입력하세요.") val email: String?,
+data class WithdrawDto(
     @field:NotBlank(message = "비밀번호를 입력하세요.") val pw: String?
 )
